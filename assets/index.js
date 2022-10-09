4ee244b4cbf02c7d4456a04b6a9e23d617dbd8f5
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-08 22:28:19 -0700

    Removed extra js and css file, along with getting local storage up and running. Cleaned up the code(removed useless code/comments and fixed spacing).

--- assets/index.js
+++ assets/index.js
@@ -5,8 +5,10 @@ const m = moment();
 let currentTense = "";
 let timeBlock = "";
 let data = "";
+//declare current time
+let currentTime = moment().hour();
 const timeArr = [
-    "9 ",
+    "9",
     "10",
     "11",
     "12",
@@ -18,103 +20,73 @@ const timeArr = [
   ];
 
 
-//declare current time
-let currentTime = moment().hour();
-
-console.log(m.format("MMMM Do YYYY, h:mm:ss a"));
-window.setInterval(function () {
-  $("#currentDay").html(moment().format("ddd MM/DD/YYYY h:mm:ss a"));
-}, 1000);
 
 
-    function changeColor(){
-        if(timeArr[i] > currentTime){
-          currentTense = "future";
-          console.log(currentTime);
-        }else if(currentTime === timeArr){
-          currentTense = "present";
-        }else{
-          currentTense = "past";
+
+    window.setInterval(function () {
+      $("#currentDay").html(moment().format("ddd MM/DD/YYYY h:mm:ss a"));
+    }, 1000);
+
+
+      function changeColor(){
+            if(timeArr[i] > currentTime){
+              currentTense = "future";
+              console.log(currentTime);
+            }else if(currentTime === timeArr){
+              currentTense = "present";
+            }else{
+              currentTense = "past";
+            }
+          
+      }
+
+    //Starts the planner
+    function runDayPlanner(){
+      for(i= 0; i<timeArr.length; i++){
+        timeBlock = moment(timeArr[i], "H").format("hA");
+
+        changeColor();
+        createRow();
+
+      }
+    }
+//Creates each row
+    function createRow(){
+        let key = "hour-"+ timeArr[i];
+        //Set the info in the textarea to blank in lieu of "null"
+        data = localStorage.getItem(key);
+        if(data == null){
+          data = "";
         }
+        var template =`
+          <div class="row time-block">
+              <div class= "hour"  "col-md-1 ">${timeBlock}</div>
+              <textarea class=" col-md-10 ${currentTense}" id="${timeArr[i]}">${data}</textarea>
+              <button data-hour="${timeArr[i]}" type="button" class="saveBtn col-md-1">Save</button>
+          </div>
+
+        `;
+        //Append the html to the container element
+        rows.append(template);  
+        
       
     }
 
-//Declare current day element
-function runDayPlanner(){
-  for(i= 0; i<timeArr.length; i++){
-    timeBlock = moment(timeArr[i], "H").format("hA");
-
-    changeColor();
-    createRow();
-
-  }
-}
-//Declare time Entries container element
-
-//render a block for each hour of the day 
-
-//for loop
-function createRow(){
-
-
-// Attempt to get the saved data for the hour of the loop
-    let key = "hour-"+ timeArr[i];
-    
-    data = localStorage.getItem(key)
-    if(data == null){
-      data = "";
-    }
-    // console.log(timeArr[i]);
-
-    var template =`
-      <div class="row time-block">
-          <div class= "hour"  "col-md-1 ">${timeBlock}</div>
-          <textarea class=" col-md-10 ${currentTense}" id="${timeArr[i]}">${data}</textarea>
-           <button data-hour="${timeArr[i]}" type="button" class="saveBtn col-md-1">Save</button>
-      </div>
-
-    `;
-    //  Append the html to the container element
-    
-     rows.append(template);  
-     
-  
-}
 
 
 
-// Save an hour to local storage
-// function saveLocalStorage(){
-//   const storage = JSON.parse(window.localStorage.getItem("data"));
-//   console.log(storage);
-//   $(".description").each(function() {
-//     const time = $(this).parent().attr("id");
-//     for(let i = 0; i < storage; i++){
-//       const dataEl = storage[i];
-//       if(time ==  dataEl.time){
-//         $(this).text(dataEl.value);
-//       }
-//     }
-//   })
-
-// }
 
 
-// Set up a "click" event listner on container
-saveBtn.on("click", "button", function(event){
-    console.log(event.target);
-    event.preventDefault();
-    const currentSaveBtn = $(even.target).data("hour");
-    localStorage.setItem(("hour-" + currentSaveBtn), $("#" + currentSaveBtn).val());
-    // const time = $(this).parent().attr("id");
-    // const textData = $(this).siblings(".description").val();
-    // const convert = JSON.parse(window.localStorage.getItem("data")) || [];
-    // convert.push({time, textData});
-    // window.localStorage.setItem("data", JSON.stringify(convert))
-    
-});
+// Set up a "click" event on container
+    rows.on("click", "button", function(event){
+        console.log(event.target);
+        event.preventDefault();
+        let currentSaveBtn = $(event.target).data("hour");
+        localStorage.setItem(("hour-" + currentSaveBtn), $("#" + currentSaveBtn).val());
+      
+    });
 
 runDayPlanner();
 
-// saveLocalStorage();
+
 
